b2085b6933e50b733926f91c6b40e443e390ec59
nnennaononuju9-max <nnennaononuju9@gmail.com>
2026-09-09 21:14:51 +0100

    Refactor import statements in callbacks.py

--- bot_handlers/callbacks.py
+++ bot_handlers/callbacks.py
@@ -28,8 +28,8 @@ from bot_handlers.menus import (
     monthly_pl_report_text,
     welcome_text,
 )
-from telegram.commands import check_channel_membership, is_admin, _community_sentiment
-from telegram.payments import (
+from bot_handlers.commands import check_channel_membership, is_admin, _community_sentiment
+from bot_hamdlers.payments import (
     handle_admin_payment_decision,
     handle_crypto_choice,
     handle_payment_method_choice,
